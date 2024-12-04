5f3376fc19432adf0f28b752b1883f0cac424a28
Domagoj <paulic.domagoj@gmail.com>
2024-12-04 07:52:48 +0100

    install vite preprocessor, run cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', vitePreprocessor())
+    },
+  },
+});
